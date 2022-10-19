534412946db6260e3a58a0f6e3e69d458ee22904
Muga <nmugasaez@gmail.com>
2022-10-19 10:56:58 -0300

    9 JOIN BETWEEN

--- queries/7_lazy-user.sql
+++ queries/7_lazy-user.sql
@@ -5,4 +5,7 @@ Seleccionar el id y el correo del usuario que no tenga ningún post
 SELECT users.id, email, title
 FROM users
 LEFT JOIN posts ON posts.user_id = users.id
-WHERE posts.title IS null; 
+--WHERE posts.title IS null; 
+
+
+---LEFT JOIN     JOIN       RIGHT JOIN
